4b22c91b9b87a5177c7d2673e1706e61867f5721
67au <oyy6745@gmail.com>
2024-02-23 00:24:16 +0800

    refactor: use var-result instead of var-dialog

---
+++ src/components/GuideResult.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { store } from '@/store';
+</script>
+
+<template>
+  <var-result class="result" :type="resultType" :title="$t(title)">
+    <template #description>
+      <template v-if="isSuccess">
+        <br>
+        <var-link type="primary" :href="href" target="_blank" text-size="16">
+          {{ $t('clickHere') }}
+        </var-link>
+      </template>
+    </template>
+    <template #footer>
+      <var-button :color="resultButtonColor" text-color="#fff" @click="click">
+        {{ $t('close') }}
+      </var-button>
+    </template>
+  </var-result>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  props: {
+    href: {
+      type: String,
+    },
+    click: {
+      type: Function,
+    }
+  },
+  computed: {
+    isSuccess() {
+      return store.guide.cache !== undefined;
+    },
+    title() {
+      return this.isSuccess ? 'found' : 'notfound';
+    },
+    resultType() {
+      return this.isSuccess ? 'success' : 'question';
+    },
+    resultButtonColor() {
+      return this.isSuccess ? 'var(--result-success-color)' : 'var(--result-question-color)';
+    }
+  }
+}
+</script>
+
+<style>
+.result {
+  width: 75vw !important;
+}
+</style>
