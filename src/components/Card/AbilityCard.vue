477975c28e277d2521f6ceac15bd97d7665e3b66
67au <oyy6745@gmail.com>
2024-02-24 14:07:24 +0800

    style: split store.codex.data into store.codex.data and store.codex.meta

--- src/components/Card/AbilityCard.vue
+++ src/components/Card/AbilityCard.vue
@@ -30,7 +30,7 @@ export default {
       return store.codex.used[category][this.offhandSkillId];
     },
     offhandSkillId() {
-      return store.codex.data['offhand_items'][store.codexPage.id];
+      return store.codex.meta['offhand_items'][store.codexPage.id];
     }
   }
 }
