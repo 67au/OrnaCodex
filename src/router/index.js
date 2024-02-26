59efc1e807b0d00d5e991e01f70b2a815805b4ee
67au <oyy6745@gmail.com>
2024-02-26 13:16:26 +0800

    feat: keep codex page scrollTop

--- src/router/index.js
+++ src/router/index.js
@@ -23,8 +23,8 @@ const router = createRouter({
   ],
   scrollBehavior(to, from, savedPosition) {
     const t = document.getElementById('main');
-    if (to['name'] === 'home') {
-      if (from['name'] === 'home') {
+    if (to.name === 'home') {
+      if (from.name === 'home') {
         t.scrollTo({
           top: 0,
           left: 0,
@@ -38,24 +38,38 @@ const router = createRouter({
         });
       }
     }
-    if (to['name'] === 'codex') {
-      t.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'auto',
-      });
-      return { el: t, top: 0, left: 0 }
+    if (to.name === 'codex') {
+      if (from.name === 'codex') {
+        const top = store.codexTop[`${to.params.category}/${to.params.id}`] || 0;
+        t.scrollTo({
+          top: top,
+          left: 0,
+          behavior: 'auto',
+        });
+        return { el: t, top: top, left: 0 }
+      } else {
+        t.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'auto',
+        });
+        return { el: t, top: 0, left: 0 }
+      }
     }
   }
 });
 
 router.afterEach((to, from) => {
-  if (from['name'] === 'home') {
+  if (from.name === 'home') {
     const t = document.getElementById('main');
     store.homeTop = t.scrollTop;
   }
-  if (to['name'] === 'codex') {
+  if (to.name === 'codex') {
     store.guide.cache = undefined;
+    if (from.name === 'codex') {
+      const t = document.getElementById('main');
+      store.codexTop[`${from.params.category}/${from.params.id}`] = t.scrollTop;
+    }
   }
 })
 
