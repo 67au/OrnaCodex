d936ffd3de776e60b8f032e1f72c194c9aaccb6a
67au <oyy6745@gmail.com>
2024-03-07 17:32:09 +0800

    chore: minor changes

--- src/components/Card/MainCard.vue
+++ src/components/Card/MainCard.vue
@@ -18,94 +18,87 @@ import { store, global } from '@/store';
     <template #description>
       <div class="card-description">
         <var-space size="mini" class="space">
-          <var-chip type="warning" size="small" :round="true" plain>{{ global.star + store.codex.usedItem['tier']
-            }}</var-chip>
+          <var-chip type="warning" size="small" :round="true" plain>
+            {{ global.star + store.codex.usedItem['tier'] }}
+          </var-chip>
           <var-chip type="primary" size="small" :round="true" plain>
             {{ $t(`categories.${store.codex.usedItem['category']}`) }}
             <template #left>
-              <var-icon name="tag" size="14" />
+              <var-icon name="bookmark" size="14" />
             </template>
           </var-chip>
 
-          <template v-if="store.codex.usedItem['exotic'] !== undefined">
-            <var-chip class="exotic" size="small" :round="true" plain v-if="store.codex.usedItem['exotic']">
-              {{ $t('exotic') }}</var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['exotic'] === true" class="exotic" size="small" :round="true" plain>
+            {{ $t('exotic') }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['rarity'] !== undefined">
-            <var-chip :class="`${store.codex.usedItem['aura']}-text`" size="small" :round="true" plain>{{
-          store.codex.usedItem['rarity'] }}</var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['rarity'] !== undefined" :class="`${store.codex.usedItem['aura']}-text`"
+            size="small" :round="true" plain>
+            {{ store.codex.usedItem['rarity'] }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['event'] !== undefined">
-            <var-chip class="highlight" size="small" :round="true" plain v-for="event in store.codex.usedItem['event']"
-              :key="event">
-              <span class="event">{{ event }}</span>
-            </var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['event'] !== undefined" class="highlight" size="small" :round="true"
+            plain v-for="event in store.codex.usedItem['event']" :key="event">
+            <span class="event">{{ event }}</span>
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['place'] !== undefined">
-            <var-chip size="small" :round="true" plain>{{ store.codex.usedItem['place'] }}</var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['place'] !== undefined" size="small" :round="true" plain>
+            {{ store.codex.usedItem['place'] }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['useable_by'] !== undefined">
-            <var-chip size="small" :round="true" plain>{{ store.codex.usedItem['useable_by'] }}</var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['useable_by'] !== undefined" size="small" :round="true" plain>
+            {{ store.codex.usedItem['useable_by'] }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['family'] !== undefined">
-            <var-chip size="small" :round="true" plain>
-              <template #left>
-                <var-icon name="card-account-details-outline" size="14" />
-              </template>
-              {{ store.codex.usedItem['family'] }}
-            </var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['family'] !== undefined" size="small" :round="true" plain>
+            <template #left>
+              <var-icon name="card-account-details-outline" size="14" />
+            </template>
+            {{ store.codex.usedItem['family'] }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['hp'] !== undefined">
-            <var-chip type="danger" size="small" :round="true" plain>
-              {{ Number(store.codex.usedItem['hp']).toLocaleString() }}
-              <template #left>
-                <var-icon name="heart" size="14" />
-              </template>
-            </var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['hp'] !== undefined" type="danger" size="small" :round="true" plain>
+            {{ Number(store.codex.usedItem['hp']).toLocaleString() }}
+            <template #left>
+              <var-icon name="heart" size="14" />
+            </template>
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['spell_type'] !== undefined">
-            <var-chip size="small" :round="true" plain>{{ `${store.codex.usedItem['spell_type']}` }}</var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['spell_type'] !== undefined" size="small" :round="true" plain>
+            {{ `${store.codex.usedItem['spell_type']}` }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['costs'] !== undefined">
-            <var-chip size="small" :round="true" plain>{{ `${store.codex.usedItem['costs']}` }}</var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['costs'] !== undefined" size="small" :round="true" plain>
+            {{ `${store.codex.usedItem['costs']}` }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['price'] !== undefined">
-            <var-chip size="small" :round="true" plain>{{ `${Number(store.codex.usedItem['price']).toLocaleString()}
-              ${$t('orns')}` }}</var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['price'] !== undefined" size="small" :round="true" plain>
+            {{ `${Number(store.codex.usedItem['price']).toLocaleString()} ${$t('orns')}` }}
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['target'] !== undefined">
-            <var-chip size="small" :round="true" plain>
-              {{ `${store.codex.usedItem['target']}` }}
-              <template #left>
-                <var-icon name="alert-circle-outline" size="14" />
-              </template>
-            </var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['target'] !== undefined" size="small" :round="true" plain>
+            {{ `${store.codex.usedItem['target']}` }}
+            <template #left>
+              <var-icon name="alert-circle-outline" size="14" />
+            </template>
+          </var-chip>
 
-          <template v-if="store.codex.usedItem['power'] !== undefined">
-            <var-chip size="small" :round="true" plain>
-              <template #left>
-                <var-icon name="fire" size="14" />
-              </template>
-              {{ `${store.codex.usedItem['power']}` }}
-            </var-chip>
-          </template>
+          <var-chip v-if="store.codex.usedItem['power'] !== undefined" size="small" :round="true" plain>
+            <template #left>
+              <var-icon name="fire" size="14" />
+            </template>
+            {{ `${store.codex.usedItem['power']}` }}
+          </var-chip>
 
           <template v-if="store.codex.usedItem['tags'] !== undefined">
             <var-chip size="small" :round="true" plain v-for="tag in store.codex.usedItem['tags']" :key="tag">
               {{ `${tag}` }}
+              <template #left>
+                <var-icon name="tag" size="14" />
+              </template>
             </var-chip>
           </template>
+
         </var-space>
       </div>
     </template>
@@ -123,3 +116,13 @@ export default {
 </script>
 
 <style src="@/styles/color.css" scoped></style>
+
+<style scoped>
+.codex-icon {
+  width: 72px;
+  height: 72px;
+  margin: 16px 0 0 16px;
+  flex-shrink: 0;
+  image-rendering: pixelated;
+}
+</style>
