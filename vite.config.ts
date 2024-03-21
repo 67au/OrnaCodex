d66aeb65b6ec41ffdcc70523e2592eeb72f302da
67au <oyy6745@gmail.com>
2024-03-21 21:59:00 +0800

    feat(ci): add TARGET env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve, dirname } from 'node:path'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import components from 'unplugin-vue-components/vite'
@@ -9,48 +9,51 @@ import autoImport from 'unplugin-auto-import/vite'
 import { VarletImportResolver } from '@varlet/import-resolver'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
-const ornaUrl = 'https://playorna.com';
-const ornaStaticUrl = `${ornaUrl}/static`;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    components({
-      resolvers: [VarletImportResolver()]
-    }),
-    autoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        {
-          '@vueuse/core': [
-            'useStorage',
-            'useDebounceFn',
-          ]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const ornaUrl = `https://${env.ORNA_URL || 'playorna.com'}`;
+  const ornaStaticUrl = `${ornaUrl}/static`;
+  return {
+    plugins: [
+      vue(),
+      components({
+        resolvers: [VarletImportResolver()]
+      }),
+      autoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          {
+            '@vueuse/core': [
+              'useStorage',
+              'useDebounceFn',
+            ]
+          },
+        ],
+        eslintrc: { enabled: true },
+        resolvers: [VarletImportResolver({ autoImport: true })]
+      }),
+      VueI18nPlugin({
+        include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locale.json'),
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: {
+      '__ORNA_URL__': JSON.stringify(ornaUrl),
+      '__ORNA_STATIC_URL__': JSON.stringify(ornaStaticUrl),
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          additionalData: `@staticUrl: '${ornaStaticUrl}';`,
         },
-      ],
-      eslintrc: { enabled: true },
-      resolvers: [VarletImportResolver({ autoImport: true })]
-    }),
-    VueI18nPlugin({
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locale.json'),
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  define: {
-    '__ORNA_URL__': JSON.stringify(ornaUrl),
-    '__ORNA_STATIC_URL__': JSON.stringify(ornaStaticUrl),
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        additionalData: `@staticUrl: '${ornaStaticUrl}';`,
       },
     },
-  },
+  }
 })
