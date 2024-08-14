9e0525a9e4bb0a0d6f7170e50a7f4bb2dce59f50
67au <oyy6745@gmail.com>
2024-08-14 16:51:55 +0800

    pref: set fonts to `Open Sans`
    Signed-off-by: 67au <oyy6745@gmail.com>

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig, presetUno } from 'unocss'
 import { presetVarlet } from '@varlet/preset-unocss'
 import presetIcons from '@unocss/preset-icons'
+import presetWebFonts from '@unocss/preset-web-fonts'
 
 export default defineConfig({
-  presets: [presetUno(), presetVarlet(), presetIcons()]
+  presets: [
+    presetUno(),
+    presetVarlet(),
+    presetIcons(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: ['Open Sans:500,700']
+      }
+    })
+  ]
 })
