2582a15982dd16b9c5e71bb51b848639d4073e90
67au <oyy6745@gmail.com>
2025-12-11 22:11:59 +0800

    pref(tower): improve `TowerTable`
    Signed-off-by: 67au <oyy6745@gmail.com>

--- src/components/TowerTable.vue
+++ src/components/TowerTable.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { mdiClose } from '@mdi/js'
+import { mdiClose, mdiCloseCircle } from '@mdi/js'
 
 const dialog = shallowRef(false)
 
-defineProps({
+const props = defineProps({
   name: {
     type: String,
   },
@@ -14,8 +14,9 @@ defineProps({
   <v-dialog
     close-on-back
     :opacity="0.1"
-    transition="fade-transition"
-    :max-width="$vuetify.display.mobile ? undefined : 600"
+    :max-width="$vuetify.display.smAndDown ? undefined : 600"
+    :transition="$vuetify.display.smAndDown ? 'dialog-bottom-transition' : 'fade-transition'"
+    :fullscreen="$vuetify.display.smAndDown"
     v-model="dialog"
     scrollable
   >
@@ -23,16 +24,27 @@ defineProps({
       <slot name="activator" :activator="activator"></slot>
     </template>
     <template v-slot:default="{ isActive }">
-      <v-card rounded>
+      <v-card :rounded="!$vuetify.display.smAndDown">
         <v-toolbar>
           <v-toolbar-title>
-            {{ name }}
+            {{ props.name }}
           </v-toolbar-title>
           <template v-slot:append>
             <div class="d-flex ga-1">
               <slot name="actions"></slot>
-              <v-btn :icon="mdiClose" variant="text" @click="isActive.value = false"></v-btn>
+              <v-btn
+                v-if="!$vuetify.display.smAndDown"
+                :icon="mdiCloseCircle"
+                @click="isActive.value = false"
+              ></v-btn>
             </div>
+          </template>
+          <template v-slot:prepend>
+            <v-btn
+              v-if="$vuetify.display.smAndDown"
+              :icon="mdiClose"
+              @click="isActive.value = false"
+            ></v-btn>
           </template>
         </v-toolbar>
 
