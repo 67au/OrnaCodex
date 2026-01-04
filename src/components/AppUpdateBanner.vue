e2af3cace8285bcc54150eb9293379e9e88cd16b
67au <oyy6745@gmail.com>
2026-01-04 23:53:29 +0800

    fix(home): disable `banner` animation
    Signed-off-by: 67au <oyy6745@gmail.com>

--- src/components/AppUpdateBanner.vue
+++ src/components/AppUpdateBanner.vue
@@ -14,22 +14,20 @@ async function reloadPage() {
 </script>
 
 <template>
-  <v-scroll-y-transition>
-    <v-banner
-      :text="$t('update.title')"
-      lines="one"
-      sticky
-      elevation="2"
-      style="top: 56px"
-      color="info"
-      v-if="show"
-    >
-      <template v-slot:prepend>
-        <v-icon color="info" :icon="mdiArrowUpBoldCircle" size="large"></v-icon>
-      </template>
-      <template v-slot:actions>
-        <v-btn color="info" :text="$t('update.refresh')" @click="reloadPage"></v-btn>
-      </template>
-    </v-banner>
-  </v-scroll-y-transition>
+  <v-banner
+    :text="$t('update.title')"
+    lines="one"
+    sticky
+    elevation="2"
+    style="top: 56px"
+    color="info"
+    v-if="show"
+  >
+    <template v-slot:prepend>
+      <v-icon color="info" :icon="mdiArrowUpBoldCircle" size="large"></v-icon>
+    </template>
+    <template v-slot:actions>
+      <v-btn color="info" :text="$t('update.refresh')" @click="reloadPage"></v-btn>
+    </template>
+  </v-banner>
 </template>
